8571220a2186ed5fab62ae24070c1bdb1696aa01
Misha Bireta <46748044+BeWoler@users.noreply.github.com>
2020-07-24 22:29:48 +0300

    Add eslint (#3)

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,15 +3,15 @@ import './main.css';
 import Card from '../card/Card';
 
 class Main extends React.Component {
-    render() {
-        return (
-            <main>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </main>
-        )
-    }
+  render() {
+    return (
+      <main>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </main>
+    );
+  }
 }
-export default Main
+export default Main;
