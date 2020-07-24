90353e1c1fd95ab8be5642276bad59f0fde640c4
unknown <bewoler.del@gmail.com>
2020-07-24 21:53:02 +0300

    Misha lentjay

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './card.css';
-import cardFirst from '../../img/card1.jpg';
 
 class Card extends React.Component {
     render() {
         return (
             <div className='card'>
-                <img className='cards-preview' src={cardFirst} alt='Preview'></img>
+                <img className='cards-preview' src={this.props.img} alt='Preview'></img>
                 <div className='text-box'>
-                    <p className='main-theme'>В Индии колдун «изгонял беса».
-                    Девушку накормили фекалиями, 
-                    а потом подожгли</p>
-                    <span className='main-text'>В индийском городе Малкангири произошел
-                    вопиющий случай во время
-                    эзотерического сеанса.</span>
+                    <p className='main-theme'>{this.props.theme}</p>
+                    <span className='main-text'>{this.props.text}</span>
                 </div>
                 <button className='button-next'>Далее</button>
             </div>
@@ -22,3 +17,4 @@ class Card extends React.Component {
 }
 
 export default Card
+ 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,14 +4,19 @@ import Card from '../card/Card';
 
 class Main extends React.Component {
     render() {
+        const cardsArr = [
+            <Card key='1' img='https://sun9-63.userapi.com/c858528/v858528247/1b4c0f/IKImJTOAhM8.jpg' theme='Первая тема' text='Первый текст' position={1} />,
+            <Card key='3' img='https://sun9-9.userapi.com/c857336/v857336360/f4c0f/AE4G0QzkAgw.jpg' theme='Третья' text='Третий текст' position={3} />,
+            <Card key='2' img='https://sun9-1.userapi.com/c858024/v858024166/160af7/HEqnJudwj5Y.jpg' theme='Вторая тема' text='Второй текст' position={2} />,
+            <Card key='4' img='https://sun9-74.userapi.com/c855724/v855724022/1d7916/slYx-TRUjHc.jpg' theme='Четвёртая' text='Четвёртый текст' position={4} />
+        ]   
+        // console.log(cardsArr[0].props.position);
         return (
             <main>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {cardsArr.sort( (a, b) => a.props.position - b.props.position )}
             </main>
         )
     }
 }
+
 export default Main
